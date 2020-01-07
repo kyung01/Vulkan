8db4dcadbd98c04e062a6d4d2e062ae786ed2c3a
BlueBug <iopzxz@gmail.com>
2020-01-08 01:40:58 +0900

    Page 22 Completed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,40 @@
-#include "Renderer.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-int main() {
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+#pragma comment(lib,"user32.lib") 
+#pragma comment(lib,"gdi32.lib") 
+#pragma comment(lib,"shell32.lib") 
+
+
+int main() { 
+	glfwInit(); 
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); 
+	
+	GLFWwindow * window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+	uint32_t extensionCount = 0;
+	
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	
+	std::cout << extensionCount << " extensions supported" << std::endl;
+	
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	
+	auto test = matrix * vec;
+	
+	while (!glfwWindowShouldClose(window)) { 
+		glfwPollEvents(); 
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
